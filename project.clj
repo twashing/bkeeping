f9d9a201252d8e737621d8eac519152a73e1176c
Timothy Washington <twashing@interruptsoftware.com>
2014-12-06 21:38:26 +0000

    -- first pass at adding verify-assertion function, after user login

--- project.clj
+++ project.clj
@@ -2,12 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.2"]
                  [ring/ring-defaults "0.1.2"]
+                 [clj-http "1.0.1"]
+                 [environ "1.0.0"]
+                 [cheshire "5.3.1"]
 
                  ;; meant to override [clj-time "0.6.0"] in ring-core
                  [clj-time "0.8.0"]]
+
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler bkeeping.core.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/bkeeping/core/handler.clj
+++ src/bkeeping/core/handler.clj
@@ -3,7 +3,10 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [ring.util.response :as ring-resp]))
+            [ring.util.response :as ring-resp]
+            [clj-http.client :as client]
+            [environ.core :refer [env]]
+            [cheshire.core :as chesr]))
 
 (defroutes app-routes
 
@@ -11,6 +14,19 @@
 
        (-> (ring-resp/response (slurp (io/resource "public/index.html")))
            (ring-resp/content-type "text/html")))
+
+  (POST "/verify-assertion" [assertion]
+
+       (let [audience (str (env :host) ":3000")  ;;"https://example.com:443"
+             response (client/post "https://verifier.login.persona.org/verify"
+                                   {:form-params {:assertion assertion
+                                                  :audience audience}})]
+
+         ;; TODO - on success
+         ;; add-user-ifnil
+         ;; set a session cookie
+         (-> (ring-resp/response (chesr/generate-string response))
+             (ring-resp/content-type "application/json"))))
 
   (route/files "/")
   (route/resources "/")
