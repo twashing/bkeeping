ded7915a673ff946746d4246a433641806937f1e
Timothy Washington <twashing@interruptsoftware.com>
2015-01-02 18:41:57 +0000

    -- abstracting transact to the cursor itself

--- resources/public/js/out/cursors.cljs
+++ resources/public/js/out/cursors.cljs
@@ -2,21 +2,36 @@
 
 
 (defprotocol ICursor
-  (-path [cursor])
-  (-state [cursor]))
+  (path [cursor])
+  (state [cursor])
+  (transact [cursor new-state]))
 
 (deftype MapCursor [state path]
   ICursor
-  (-path [cursor] path)
-  (-state [cursor] state))
+  (path [cursor] path)
+  (state [cursor] state)
+  (transact [cursor new-state]
+    (swap! state
+           (fn [e]
+             (update-in e path (fn [_] new-state))))))
 
 (defn cursor [state path]
   (cond
    (map? @state) (MapCursor. state path)))
 
-(defn transact [cursor new-state]
-  (let [old-state (.-state cursor)
-        path (.-path cursor)]
-    (swap! old-state
-           (fn [e]
-             (update-in e path (fn [_] new-state))))))
+
+(comment
+
+  (def state (atom {:a 1 :b {:c 3 :d {:e 5}}}))
+
+  (def one (cursor state [:b :c]))
+  (.transact one 4)
+  (.state one)
+
+  (def two (cursor state [:b :d]))
+  (.transact two {:foo :bar})
+  (.state two)
+
+  (type two)
+
+  )
--- resources/public/js/out/cursors.js
+++ resources/public/js/out/cursors.js
@@ -2,59 +2,85 @@
 goog.provide('cursors');
 goog.require('cljs.core');
 
-cursors.ICursor = (function (){var obj20735 = {};
-return obj20735;
+cursors.ICursor = (function (){var obj20773 = {};
+return obj20773;
 })();
 
-cursors._path = (function _path(cursor){
+cursors.path = (function path(cursor){
 if((function (){var and__12533__auto__ = cursor;
 if(and__12533__auto__){
-return cursor.cursors$ICursor$_path$arity$1;
+return cursor.cursors$ICursor$path$arity$1;
 } else {
 return and__12533__auto__;
 }
 })()){
-return cursor.cursors$ICursor$_path$arity$1(cursor);
+return cursor.cursors$ICursor$path$arity$1(cursor);
 } else {
 var x__13189__auto__ = (((cursor == null))?null:cursor);
-return (function (){var or__12545__auto__ = (cursors._path[goog.typeOf(x__13189__auto__)]);
+return (function (){var or__12545__auto__ = (cursors.path[goog.typeOf(x__13189__auto__)]);
 if(or__12545__auto__){
 return or__12545__auto__;
 } else {
-var or__12545__auto____$1 = (cursors._path["_"]);
+var or__12545__auto____$1 = (cursors.path["_"]);
 if(or__12545__auto____$1){
 return or__12545__auto____$1;
 } else {
-throw cljs.core.missing_protocol.call(null,"ICursor.-path",cursor);
+throw cljs.core.missing_protocol.call(null,"ICursor.path",cursor);
 }
 }
 })().call(null,cursor);
 }
 });
 
-cursors._state = (function _state(cursor){
+cursors.state = (function state(cursor){
 if((function (){var and__12533__auto__ = cursor;
 if(and__12533__auto__){
-return cursor.cursors$ICursor$_state$arity$1;
+return cursor.cursors$ICursor$state$arity$1;
 } else {
 return and__12533__auto__;
 }
 })()){
-return cursor.cursors$ICursor$_state$arity$1(cursor);
+return cursor.cursors$ICursor$state$arity$1(cursor);
 } else {
 var x__13189__auto__ = (((cursor == null))?null:cursor);
-return (function (){var or__12545__auto__ = (cursors._state[goog.typeOf(x__13189__auto__)]);
+return (function (){var or__12545__auto__ = (cursors.state[goog.typeOf(x__13189__auto__)]);
 if(or__12545__auto__){
 return or__12545__auto__;
 } else {
-var or__12545__auto____$1 = (cursors._state["_"]);
+var or__12545__auto____$1 = (cursors.state["_"]);
 if(or__12545__auto____$1){
 return or__12545__auto____$1;
 } else {
-throw cljs.core.missing_protocol.call(null,"ICursor.-state",cursor);
+throw cljs.core.missing_protocol.call(null,"ICursor.state",cursor);
 }
 }
 })().call(null,cursor);
+}
+});
+
+cursors.transact = (function transact(cursor,new_state){
+if((function (){var and__12533__auto__ = cursor;
+if(and__12533__auto__){
+return cursor.cursors$ICursor$transact$arity$2;
+} else {
+return and__12533__auto__;
+}
+})()){
+return cursor.cursors$ICursor$transact$arity$2(cursor,new_state);
+} else {
+var x__13189__auto__ = (((cursor == null))?null:cursor);
+return (function (){var or__12545__auto__ = (cursors.transact[goog.typeOf(x__13189__auto__)]);
+if(or__12545__auto__){
+return or__12545__auto__;
+} else {
+var or__12545__auto____$1 = (cursors.transact["_"]);
+if(or__12545__auto____$1){
+return or__12545__auto____$1;
+} else {
+throw cljs.core.missing_protocol.call(null,"ICursor.transact",cursor);
+}
+}
+})().call(null,cursor,new_state);
 }
 });
 
@@ -68,16 +94,30 @@ this.path = path;
 })
 cursors.MapCursor.prototype.cursors$ICursor$ = true;
 
-cursors.MapCursor.prototype.cursors$ICursor$_path$arity$1 = (function (cursor){
+cursors.MapCursor.prototype.cursors$ICursor$path$arity$1 = (function (cursor){
 var self__ = this;
 var cursor__$1 = this;
 return self__.path;
 });
 
-cursors.MapCursor.prototype.cursors$ICursor$_state$arity$1 = (function (cursor){
+cursors.MapCursor.prototype.cursors$ICursor$state$arity$1 = (function (cursor){
 var self__ = this;
 var cursor__$1 = this;
 return self__.state;
+});
+
+cursors.MapCursor.prototype.cursors$ICursor$transact$arity$2 = (function (cursor,new_state){
+var self__ = this;
+var cursor__$1 = this;
+return cljs.core.swap_BANG_.call(null,self__.state,((function (cursor__$1){
+return (function (e){
+return cljs.core.update_in.call(null,e,self__.path,((function (cursor__$1){
+return (function (_){
+return new_state;
+});})(cursor__$1))
+);
+});})(cursor__$1))
+);
 });
 
 cursors.MapCursor.cljs$lang$type = true;
@@ -98,19 +138,6 @@ return (new cursors.MapCursor(state,path));
 } else {
 return null;
 }
-});
-cursors.transact = (function transact(cursor,new_state){
-var old_state = cursor.state;
-var path = cursor.path;
-return cljs.core.swap_BANG_.call(null,old_state,((function (old_state,path){
-return (function (e){
-return cljs.core.update_in.call(null,e,path,((function (old_state,path){
-return (function (_){
-return new_state;
-});})(old_state,path))
-);
-});})(old_state,path))
-);
 });
 
 //# sourceMappingURL=cursors.js.map
