bff0e0d368c61b1f5a1a96c3830107ebf0e2a8d1
Timothy Washington <twashing@interruptsoftware.com>
2015-01-15 18:11:14 +0000

    -- moved clojuresctipt source files under src

---
+++ src/cljs/clojurescript/index.cljs
@@ -0,0 +1,27 @@
+(ns index
+  (:require [cljs.reader :as reader]
+            [goog.events :as events]
+            [goog.dom :as gdom]
+            [weasel.repl :as ws-repl]
+            [bkeeping :as bk])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
+
+
+(defn onClickHandler []
+  (let [currentUser "twashing@gmail.com"
+        navigatorId js/navigator.id]
+
+    (bk/console-log "Signin CLICKED")
+    (.watch navigatorId
+            (clj->js {:loggedInUser currentUser
+                      :onlogin bk/verifyAssertion
+                      :onlogout bk/signoutUser}))
+    (.request navigatorId)))
+
+(defn start []
+  (if-let [signinLink (gdom/getElement "signin")]
+    (set! (.-onclick signinLink) onClickHandler)))
+
+(start)
